5a4e7808cdac0629371fde89723784d6553e3829
Arjun Varma <junky.argonaut@gmail.com>
2013-03-06 23:46:00 +0530

    Partial Feature Extraction with minor bug

---
+++ FExtractor.py
@@ -0,0 +1,21 @@
+import BasicStats as bs
+
+'''
+The structure of the sentence feature set basic is as follows
+[t_line_num, length, isQuestion, t_rel_pos]
+'''
+
+def s_extractor(s_features, count):
+    for name in s_features:
+        for s in s_features[name]:
+            l = s_features[name][s][0]
+            s_features[name][s].append(l/count)
+    print s_features
+
+def main():
+    thread_dict, s_features, count = bs.read_xml()
+    #bs.print_Stats(thread_dict)
+    s_extractor(s_features, count)
+
+if __name__ == '__main__':
+    main()
